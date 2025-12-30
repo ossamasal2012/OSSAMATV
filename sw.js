d444877a34e0aaf1b4bf91ec13d607a5bb15640c
ossamasal2012 <ossamasal2012@gmail.com>
2025-12-30 22:16:56 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,3 +1,27 @@
-self.addEventListener('fetch', function(event) {
-  // هذا الكود يسمح للمتصفح بالتعرف على الموقع كـ PWA
+const CACHE_NAME = 'ossama-tv';
+const ASSETS = [
+  './',
+  './index.html',
+  'https://i.ibb.co/zh1ky6SN/logo2.png'
+];
+
+// تثبيت الـ Service Worker
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS);
+    })
+  );
+});
+
+// تفعيل الـ Service Worker
+self.addEventListener('activate', (event) => {
+  console.log('Service Worker activated');
+});
+
+// الاستجابة للطلبات (ضروري لظهور أيقونة التثبيت)
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    fetch(event.request).catch(() => caches.match(event.request))
+  );
 });
