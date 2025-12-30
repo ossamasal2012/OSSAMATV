db1cb0d40ea67e6a9e97c474fc9e84b82dbdb22e
ossamasal2012 <ossamasal2012@gmail.com>
2025-12-30 22:22:20 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,27 +1,8 @@
-const CACHE_NAME = 'ossama-tv';
-const ASSETS = [
-  './',
-  './index.html',
-  'https://i.ibb.co/zh1ky6SN/logo2.png'
-];
-
-// تثبيت الـ Service Worker
-self.addEventListener('install', (event) => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => {
-      return cache.addAll(ASSETS);
-    })
-  );
-});
-
-// تفعيل الـ Service Worker
-self.addEventListener('activate', (event) => {
-  console.log('Service Worker activated');
-});
-
-// الاستجابة للطلبات (ضروري لظهور أيقونة التثبيت)
-self.addEventListener('fetch', (event) => {
-  event.respondWith(
-    fetch(event.request).catch(() => caches.match(event.request))
-  );
-});
+<script>
+  if ('serviceWorker' in navigator) {
+    // استخدام './sw.js' بدلاً من المسار الكامل يضمن العثور على الملف داخل مجلد المشروع
+    navigator.serviceWorker.register('./sw.js')
+      .then(() => console.log("Service Worker Registered"))
+      .catch((err) => console.log("Service Worker Failed", err));
+  }
+</script>
